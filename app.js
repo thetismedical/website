059f50e7b5d3823f5b7d56fd1fda01bcf93d30fa
Guy Solan <thetismedical@gmail.com>
2021-09-23 13:15:33 +0100

    seo fixes screen-width

--- app.js
+++ app.js
@@ -150,83 +150,16 @@ function navToggle(e) {
       y: 0,
       background: "black"
     });
-    gsap.to("#logo", 1, {
+    gsap.to("#logo", 0.5, {
       color: "black"
     });
-    gsap.to(".nav-bar", 1, {
-      clipPath: "circle(50px at 100% -10%)"
+    gsap.to(".nav-bar", 0.5, {
+      clipPath: "circle(0px at 88% 8%)"
     });
     document.body.classList.remove("hide");
   }
 }
 
-//Barba Page Transitions
-// const logo = document.querySelector("#logo");
-// barba.init({
-//   views: [{
-//       namespace: "home",
-//       beforeEnter() {
-//         animateSlides();
-//         logo.href = "./index.html";
-//       },
-//       beforeLeave() {
-//         slideScene.destroy();
-//         pageScene.destroy();
-//         controller.destroy();
-//       }
-//     },
-//     {
-//       namespace: "fashion",
-//       beforeEnter() {
-//         logo.href = "../index.html";
-//         detailAnimation();
-//       },
-//       beforeLeave() {
-//         controller.destroy();
-//         detailScene.destroy();
-//       }
-//     }
-//   ],
-//   // transitions: [
-//   //   {
-//   //     leave({ current, next }) {
-//   //       let done = this.async();
-//   //       //An Animation
-//   //       const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
-//   //       tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
-//   //       tl.fromTo(
-//   //         ".swipe",
-//   //         0.75,
-//   //         { x: "-100%" },
-//   //         { x: "0%", onComplete: done },
-//   //         "-=0.5"
-//   //       );
-//   //     },
-//   //     enter({ current, next }) {
-//   //       let done = this.async();
-//   //       //Scroll to the top
-//   //       window.scrollTo(0, 0);
-//   //       //An Animation
-//   //       const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
-//   //       tl.fromTo(
-//   //         ".swipe",
-//   //         1,
-//   //         { x: "0%" },
-
-//   //         { x: "100%", stagger: 0.2, onComplete: done }
-//   //       );
-//   //       tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
-//   //       tl.fromTo(
-//   //         ".nav-header",
-//   //         1,
-//   //         { y: "-100%" },
-//   //         { y: "0%", ease: "power2.inOut" },
-//   //         "-=1.5"
-//   //       );
-//   //     }
-//   //   }
-//   // ]
-// });
 
 // function detailAnimation() {
 //   controller = new ScrollMagic.Controller();
@@ -272,6 +205,78 @@ function navToggle(e) {
 //       .addTo(controller);
 //   });
 // }
+
+
+//Barba Page Transitions
+// const logo = document.querySelector("#logo");
+
+// barba.init({
+//   views: [{
+//       namespace: "home",
+//       beforeEnter() {
+//         // animateSlides();
+//         // logo.href = "./";
+//       },
+//       beforeLeave() {
+//         // slideScene.destroy();
+//         // pageScene.destroy();
+//         // controller.destroy();
+//       }
+//     },
+//     {
+//       namespace: "splint",
+//       beforeEnter() {
+//         // logo.href = "./night-splint.html";
+//         // detailAnimation();
+//       },
+//       beforeLeave() {
+//         // controller.destroy();
+//         // detailScene.destroy();
+//       }
+//     }
+//   ],
+//   transitions: [
+//     {
+//       leave({ current, next }) {
+//         let done = this.async();
+//         //An Animation
+//         const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+//         tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+//         tl.fromTo(
+//           ".swipe",
+//           0.75,
+//           { width: "0%" },
+//           { width: "100%", onComplete: done },
+//           "-=0.75"
+//         );
+//       },
+//       enter({ current, next }) {
+//         let done = this.async();
+//         //Scroll to the top
+//         window.scrollTo(0, 0);
+//         //An Animation
+//         const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+//         tl.fromTo(
+//           ".swipe",
+//           1,
+//           { width: "0%" },
+
+//           { width: "100%", stagger: 0.2, onComplete: done }
+//         );
+//         tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+//         tl.fromTo(
+//           ".nav-header",
+//           1,
+//           { y: "-100%" },
+//           { y: "0%", ease: "power2.inOut" },
+//           "-=1"
+//         );
+//       }
+//     }
+//   ]
+// });
+
+
 //EventListeners
 burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
