fdce13835cd4b9154c84bd8f25b0895d4f6995dd
Guy Solan <thetismedical@gmail.com>
2022-03-11 14:09:01 +0000

    component time

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 function copyToClipboard() {
   var inputc = document.body.appendChild(document.createElement("input"));
   inputc.value = window.location.href;
@@ -62,60 +64,6 @@ function navToggle(e) {
     navbar.classList.add("active")
   }
 }
-
-
-// function navToggle(e) {
-
-//   if (!e.target.classList.contains("active")) {
-//     e.target.classList.add("active");
-//     gsap.to(".line1", 0.25, {
-//       rotate: "45",
-//       y: 5,
-//       background: "white"
-//     });
-//     gsap.to(".line2", 0.25, {
-//       rotate: "-45",
-//       y: -5,
-//       background: "white"
-//     });
-//     gsap.to("#logo", 1, {
-//       color: "white"
-//     });
-//     gsap.to(".nav-bar", 1, {
-//       clipPath: "circle(3500px at 100% -10%)"
-//     });
-//     document.body.classList.add("hide");
-//   } else {
-//     e.target.classList.remove("active");
-//     gsap.to(".line1", 0.25, {
-//       rotate: "0",
-//       y: 0,
-//       background: "black"
-//     });
-//     gsap.to(".line2", 0.25, {
-//       rotate: "0",
-//       y: 0,
-//       background: "black"
-//     });
-//     gsap.to("#logo", 0.5, {
-//       color: "black"
-//     });
-//     gsap.to(".nav-bar", 0.5, {
-//       clipPath: "circle(0px at 88% 8%)"
-//     });
-//     document.body.classList.remove("hide");
-//   }
-//   if(navbar.classList.contains("active")){
-//     setTimeout(function() {
-//       navbar.classList.remove("active")
-//     }, 500);
-    
-//   }
-//   else{
-//     navbar.classList.add("active")
-//   }
-// }
-
 
 //EventListeners
 burger.addEventListener("click", navToggle);
