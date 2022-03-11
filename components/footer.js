5e39f372614f6332df2e98150954bc5a985ca3b1
Guy Solan <thetismedical@gmail.com>
2022-03-11 16:09:13 +0000

    links in footer

--- components/footer.js
+++ components/footer.js
@@ -5,28 +5,56 @@ class Footer extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <section id='contact' class='footer'>
-      <div class="footer-content">
-         <div class='footer-words'>
-            <div class='footer-title'>
-               <h2>Contact</h2>
-               <img src="../images/logo.svg" alt="Logo Image">
-            </div>
-            <div class="socials">
-               <a class='invert' target="_blank"
-                  href='https://us5.list-manage.com/contact-form?u=c24150147626fd5e19c7684c6&form_id=06069b6e5471c060d76fb6ebe2d5adc7'><img
-                     src="../images/social-media-email.svg" alt="Email Icon"></a>
-               <a class='invert' href='https://www.linkedin.com/company/thetis-medical-ltd'><img
-                     src="../images/social-media-linkedin.svg" alt="Linkedin Icon"></a>
-               <a class='invert' href='https://www.instagram.com/thetismedical/'><img
-                     src="../images/social-media-insta.svg" alt="Instagram Icon"></a>
-               <a class='invert' href='https://www.facebook.com/thetismedical'><img
-                     src="../images/social-media-facebook.svg" alt="Facebook Icon"></a>
-            </div>
-            <p>&copy;Thetis Medical Ltd 2022</p>
-         </div>
-      </div>
-   </section>
+    <section id='contact' class='footer'>
+    <div class="footer-content">
+       <div class='footer-words'>
+          <div class='footer-title'>
+             <h2>Contact</h2>
+             <img src="../images/logo.svg" alt="Logo Image">
+          </div>
+          <div class="links-row">
+             <div class="links-column">
+                <h3>Company</h3>
+                <a href="../">Home</a>
+                <a href="../contact.html">Contact Us</a>
+                <h3>Products</h3>
+                <a href="../night-splint.html">Achilles Rupture Night Splint</a>
+                <a href="../trauma-splint.html">Achilles Rupture Trauma Splint</a>
+             </div>
+             <div class="links-column">
+                <h3>Information</h3>
+                <a href="../achilles-ruptures.html">Achilles Ruptures</a>
+                <a href="../FAQs/achilles-rupture-timeline.html">Achilles Rupture Timeline</a>
+                <a href="../FAQs/is-my-achilles-ruptured.html">Is my Achilles Ruptured?</a>
+                <a href="../FAQs/what-happens-if-my-achilles-is-ruptured?">What happens if my Achilles is ruptured?</a>
+                <a href="../FAQs/achilles-tear-treatment.html">Achilles Tear Treatment</a>
+                <a href="../FAQs/torn-achilles-recovery.html">Torn Achilles Recovery</a>
+                <a href="../FAQs/life-after-achilles-rupture.html">Life After Achilles Rupture</a>
+             </div>
+             <div class="links-column">
+                <h3>Stay in Touch</h3>
+                <a class='invert' target="_blank"
+                href='mailto:info@thetismedical.com'>Email Us</a>
+             <a class='invert' href='https://www.linkedin.com/company/thetis-medical-ltd'>LinkedIn</a>
+             <a class='invert' href='https://www.instagram.com/thetismedical/'>Instagram</a>
+             <a class='invert' href='https://www.facebook.com/thetismedical'>Facebook</a>
+             </div>
+          </div>
+          <div class="socials">
+             <a class='invert' target="_blank"
+                href='mailto:info@thetismedical.com'><img
+                   src="../images/social-media-email.svg" alt="Email Icon"></a>
+             <a class='invert' href='https://www.linkedin.com/company/thetis-medical-ltd'><img
+                   src="../images/social-media-linkedin.svg" alt="Linkedin Icon"></a>
+             <a class='invert' href='https://www.instagram.com/thetismedical/'><img
+                   src="../images/social-media-insta.svg" alt="Instagram Icon"></a>
+             <a class='invert' href='https://www.facebook.com/thetismedical'><img
+                   src="../images/social-media-facebook.svg" alt="Facebook Icon"></a>
+          </div>
+          <p>&copy;Thetis Medical Ltd 2022</p>
+       </div>
+    </div>
+ </section>
       `;
   }
 }
