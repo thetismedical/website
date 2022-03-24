2880ce98421a5934e74afc0d33fb26efcd3844a2
Guy Solan <thetismedical@gmail.com>
2022-03-24 14:11:02 +0000

    did it work?

--- app.js
+++ app.js
@@ -52,13 +52,12 @@ function navToggle(e) {
     });
     document.body.classList.remove("hide");
   }
-  if(navbar.classList.contains("active")){
-    setTimeout(function() {
+  if (navbar.classList.contains("active")) {
+    setTimeout(function () {
       navbar.classList.remove("active")
     }, 500);
-    
-  }
-  else{
+
+  } else {
     navbar.classList.add("active")
   }
 }
@@ -68,21 +67,21 @@ burger.addEventListener("click", navToggle);
 orderCheck = document.getElementById('order-check')
 orderWrap = document.getElementsByClassName('order-wrap')[0]
 
-try{
+try {
   orderCheck.addEventListener("click", hideOrder);
 
   function hideOrder() {
-      if (orderWrap.classList.contains('hide')) {
-          orderWrap.classList.remove('hide')
-      } else {
-          orderWrap.classList.add('hide')
-      }
+    if (orderWrap.classList.contains('hide')) {
+      orderWrap.classList.remove('hide')
+    } else {
+      orderWrap.classList.add('hide')
+    }
   }
-} catch(e){
+} catch (e) {
   console.log('Not Contact Us')
 }
 
-function loadGSAP(){
+function loadGSAP() {
   let script = document.createElement('script');
   script.setAttribute('type', 'text/javascript');
   script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/gsap.min.js');
@@ -91,13 +90,36 @@ function loadGSAP(){
   document.head.appendChild(script);
 }
 
-function loadMailchimp(){
-  console.log('loading mailchimp')
-  let script = document.createElement('script');
-  script.setAttribute('type', 'text/javascript');
-  script.setAttribute('src', '//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js');
-  document.head.appendChild(script);
+function loadMailchimp() {
+  validateMailchimp()
+  connectMailchimp()
 }
 
-const mailchimpInput = document.getElementById('mce-EMAIL') 
+const mailchimpInput = document.getElementById('mce-EMAIL')
 mailchimpInput.addEventListener("click", loadMailchimp);
+
+function validateMailchimp() {
+  let validate = document.getElementById('valmc')
+  if (validate == null) {
+    console.log('validating mailchimp')
+    let script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('id', 'valmc');
+    script.setAttribute('src', '//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js');
+    document.head.appendChild(script);
+  }
+}
+
+
+function connectMailchimp() {
+  let connected = document.getElementById('mjcs')
+  if (connected == null) {
+    console.log('connecting mailchimp')
+    let script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('id', 'mjcs');
+    let mcjsScript = `! function (c, h, i, m, p) { m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore( m, p)}(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/c24150147626fd5e19c7684c6/d7e07f4cbe8075f8afbf34db0.js");`
+    script.setAttribute('innerHTML', mcjsScript);
+    document.head.appendChild(script);
+  }
+}
