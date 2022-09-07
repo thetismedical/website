b1147d9094b91f90efc1dac1adbf635e4d0c5a84
Guy Solan <thetismedical@gmail.com>
2022-09-07 23:51:26 +0100

    US Attributuion tags

--- components/location-dev.js
+++ components/location-dev.js
@@ -2,22 +2,29 @@ let toUS = ['United States','Canada','Mexico']
 
 let US_URL = 'https://www.amazon.com/dp/B09N5KH4F3?ref=myi_title_dp'
 
-fetch('https://extreme-ip-lookup.com/json/?key=mRicTdUafjFRd3Ufqftq')
+const urls = {
+    US_home: 'https://www.amazon.com/dp/B09N5MVY1Q?maas=maas_adg_75CEE5FF68E8B03C603B001959199F40_afap_abs&ref_=aa_maas&tag=maas&ref=myi_title_dp&th=1',
+    US_product: 'https://www.amazon.com/dp/B09N5MVY1Q?maas=maas_adg_CDB334A832298E5FAEF6F3F7FB3B9DC7_afap_abs&ref_=aa_maas&tag=maas&ref=myi_title_dp&th=1',
+}
 
+fetch('https://extreme-ip-lookup.com/json/?key=mRicTdUafjFRd3Ufqftq')
 .then( res => res.json())
 .then(response => {
     console.log("Country: ", response.country);
     try {
+        // Check if on Night Splint Page
         if (window.location.href.indexOf("night-splint") > -1) {
+            console.log("product")
             if (toUS.includes(response.country)){
-                let link = document.getElementById('order-night-product')
-                link.setAttribute("href", US_URL);
-            } else {
+                let product_link = document.getElementById('order-night-product')
+                product_link.setAttribute("href", urls.US_product);
             }
+        // If not on Night Splint Page
         } else {
             if (!toUS.includes(response.country)){
+                console.log("home")
                 let link = document.getElementById('order-night-home')
-                link.setAttribute("href", UK_URL);
+                link.setAttribute("href", urls.US_home);
             }
         }
         
--- components/location.js
+++ components/location.js
@@ -1,1 +1,39 @@
-let toUS=["United States","Canada","Mexico"],US_URL="https://www.amazon.com/dp/B09N5KH4F3?ref=myi_title_dp";fetch("https://extreme-ip-lookup.com/json/?key=mRicTdUafjFRd3Ufqftq").then(e=>e.json()).then(e=>{console.log("Country: ",e.country);try{if(window.location.href.indexOf("night-splint")>-1)if(toUS.includes(e.country)){console.log(".com"),document.getElementById("order-night-product").setAttribute("href",US_URL)}else console.log(".co.uk");else if(!toUS.includes(e.country)){console.log(".co.uk"),document.getElementById("order-night-home").setAttribute("href",UK_URL)}}catch(e){console.log(e)}}).catch((e,t)=>{console.log("Request failed")});
+let toUS = ['United States','Canada','Mexico']
+
+let US_URL = 'https://www.amazon.com/dp/B09N5KH4F3?ref=myi_title_dp'
+
+const urls = {
+    US_home: 'https://www.amazon.com/dp/B09N5MVY1Q?maas=maas_adg_75CEE5FF68E8B03C603B001959199F40_afap_abs&ref_=aa_maas&tag=maas&ref=myi_title_dp&th=1',
+    US_product: 'https://www.amazon.com/dp/B09N5MVY1Q?maas=maas_adg_CDB334A832298E5FAEF6F3F7FB3B9DC7_afap_abs&ref_=aa_maas&tag=maas&ref=myi_title_dp&th=1',
+}
+
+fetch('https://extreme-ip-lookup.com/json/?key=mRicTdUafjFRd3Ufqftq')
+.then( res => res.json())
+.then(response => {
+    console.log("Country: ", response.country);
+    try {
+        // Check if on Night Splint Page
+        if (window.location.href.indexOf("night-splint") > -1) {
+            console.log("product")
+            if (toUS.includes(response.country)){
+                let product_link = document.getElementById('order-night-product')
+                product_link.setAttribute("href", urls.US_product);
+            }
+        // If not on Night Splint Page
+        } else {
+            if (!toUS.includes(response.country)){
+                console.log("home")
+                console.log("US URL: ",urls.US_home)
+                let link = document.getElementById('order-night-home')
+                link.setAttribute("href", urls.US_home);
+            }
+        }
+
+    } catch (error) {
+        // console.log(error)
+    }
+
+ })
+ .catch((data, status) => {
+    // console.log('Request failed');
+ })
